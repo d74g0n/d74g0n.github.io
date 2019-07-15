643dc22900c6a925f5476a8f30e89268915c7020
d74g0n (dragon) <d74g0n@gmail.com>
2019-07-15 01:50:40 -0400

    upgrayd
    upgrayd

---
+++ datafag/toggle_vis.js
@@ -0,0 +1,10 @@
+function toggle_MSN_vis() {
+
+    if (document.getElementById('frameheader').style.visibility == "hidden") {
+        document.getElementById('frameheader').style.visibility = "visible";
+    } else {
+
+        document.getElementById('frameheader').style.visibility = "hidden"
+    }
+
+}
