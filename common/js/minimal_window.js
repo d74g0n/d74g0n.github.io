5a376b45973794c7c521a75c41071ce27cd63339
d74g0n (dragon) <d74g0n@gmail.com>
2019-07-24 04:48:33 -0400

    clean up
    clean up

---
+++ common/js/minimal_window.js
@@ -0,0 +1,17 @@
+        function openMinWindow(requrl) {
+            let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=600,height=300,left=100,top=100`;
+
+            open(requrl, 'test', params);
+
+        }
+
+
+        function openMinWindowId(requrl = '/', reqid = 'test') {
+            let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=600,height=300,left=100,top=100`;
+
+            open(requrl, reqid, params);
+
+        }
+
