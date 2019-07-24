1014385bf2bf0fdf3263fec61ba362cfc997e258
d74g0n (dragon) <d74g0n@gmail.com>
2019-07-24 13:50:30 -0400

    minimal window
    minified window

--- common/js/minimal_window.js
+++ common/js/minimal_window.js
@@ -1,17 +1,17 @@
-        function openMinWindow(requrl) {
+
+// opens the 'popout' browser form with a url.  a minified browser window.
+
+function openMinWindow(requrl) {
             let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
 width=600,height=300,left=100,top=100`;
 
             open(requrl, 'test', params);
-
         }
-
 
         function openMinWindowId(requrl = '/', reqid = 'test') {
             let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
 width=600,height=300,left=100,top=100`;
 
             open(requrl, reqid, params);
-
         }
 
