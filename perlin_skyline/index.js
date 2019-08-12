119e463cbcc330c59feab444b1568bc7e6ec7597
d74g0n (dragon) <d74g0n@gmail.com>
2019-08-12 02:47:52 -0400

    added loop
    looping now

--- perlin_skyline/index.js
+++ perlin_skyline/index.js
@@ -1,60 +1,68 @@
+function drawcity() {
+
     function WIP(x = -10, w = 20) {
 
-            //            let w = 10;
-            let h = 180;
-            let minBheight = 30;
-            buildingABasic(x, w, h - RNDNum(0, h - minBheight));
-        }
-        background(gradientV());
-        horizontalLine(canvas.height * 0.7);
+        //            let w = 10;
+        let h = 180;
+        let minBheight = 30;
+        buildingABasic(x, w, h - RNDNum(0, h - minBheight));
+    }
+    background(gradientV());
+//    horizontalLine(canvas.height * 0.7);
 
 
-        let w = 10 + RNDNum(5, 15);
+    let w = 10 + RNDNum(5, 15);
 
-        for (let x = (RNDNum(-25, -5)); x < canvas.width; x += w + 1) {
-            w = 10 + RNDNum(5, 15);
-            WIP(x, w);
-            //            WIP(x,(RNDNum(10, 30)));
-        }
+    for (let x = (RNDNum(-25, -5)); x < canvas.width; x += w + 1) {
+        w = 10 + RNDNum(5, 15);
+        WIP(x, w);
+        //            WIP(x,(RNDNum(10, 30)));
+    }
 
-        setColor('black');
-
-
-        ctx.globalAlpha = 0.5;
-
-        setColor('black');
-        drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.2);
-
-        background(gradientV());
-        ctx.globalAlpha = 1;
-
-        GD.horizonY += 2;
-
-        for (let x = (RNDNum(-25, -5)); x < canvas.width; x += w + 1) {
-            w = 10 + RNDNum(5, 15);
-            WIP(x, w);
-            //            WIP(x,(RNDNum(10, 30)));
-        }
-
-        setColor('black');
+    setColor('black');
 
 
-        ctx.globalAlpha = 0.5;
+    ctx.globalAlpha = 0.5;
 
-        setColor('black');
-        drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.2);
+    setColor('black');
+    drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.2);
 
-        background(gradientV());
-        ctx.globalAlpha = 1;
+    background(gradientV());
+    ctx.globalAlpha = 1;
 
-        GD.horizonY += 5;
+    GD.horizonY += 2;
+
+    for (let x = (RNDNum(-25, -5)); x < canvas.width; x += w + 1) {
+        w = 10 + RNDNum(5, 15);
+        WIP(x, w);
+        //            WIP(x,(RNDNum(10, 30)));
+    }
+
+    setColor('black');
 
 
-        for (let x = (RNDNum(-20, -10)); x < canvas.width; x += w + 2) {
-            w = 10 + RNDNum(5, 15);
-            WIP(x, w);
+    ctx.globalAlpha = 0.5;
 
-            //              WIP(x,(RNDNum(10, 30)));
-        }
-        setColor('black');
-        drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.3);
+    setColor('black');
+    drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.2);
+
+    background(gradientV());
+    ctx.globalAlpha = 1;
+
+    GD.horizonY += 5;
+
+
+    for (let x = (RNDNum(-20, -10)); x < canvas.width; x += w + 2) {
+        w = 10 + RNDNum(5, 15);
+        WIP(x, w);
+
+        //              WIP(x,(RNDNum(10, 30)));
+    }
+    setColor('black');
+    drawRect(0, GD.horizonY, canvas.width, canvas.height * 0.3);
+    
+    GD.horizonY -= 7;
+}
+
+setInterval(drawcity, 1000/3);
+drawcity();
