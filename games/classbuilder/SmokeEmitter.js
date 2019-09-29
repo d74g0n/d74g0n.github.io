9d57a3049cbe90878f97f20ffe58af1750b1fb81
d74g0n (dragon) <d74g0n@gmail.com>
2019-09-29 06:10:29 -0400

    SmokeEmitter v1
    SmokeEmitter v1

---
+++ games/classbuilder/SmokeEmitter.js
@@ -0,0 +1,196 @@
+// TODO: merge with tinter.
+
+class SmokeEmitter {
+    constructor(x, y, amount, emitRate = 1) {
+        this.id = `SmokeEmitter`;
+        this.x = x;
+        this.y = y;
+        this.amount = amount;
+        this.emitted = [];
+        this.isOn = true;
+        this.framecount = 0;
+        this.frameMod = 15;
+        this.puffcount = 0;
+        this.emitRate = 1;
+        this.wind = 0;
+    }
+    
+    resetWind(){
+        this.setWind();
+    }
+
+    setWind(val = 0) {
+        this.wind = val;
+        for (let i = 0; i < this.emitted.length; i++) {
+            this.emitted[i].wind = this.wind;
+        }
+    }
+
+    emit(val = this.amount) {
+        return this.takePuff(val);
+    }
+
+    takePuff(val = this.amount) { // smoking cigarette context
+        for (let i = 0; i <= val; i++) {
+            this.emitted.unshift(this.addParticle());
+        }
+    }
+
+    addParticle() {
+        return new Smoke(this.x, this.y, smokeImage);
+    }
+
+    doCalculations() {
+        this.framecount++;
+        if (this.framecount > 100) {
+            this.framecount = 1;
+            this.puffcount++;
+        }
+        if (this.puffcount > 100) {
+            this.puffcount = 1;
+        }
+    }
+
+    cleanup() {
+        for (let i = 0; i < this.emitted.length; i++) {
+            if (this.emitted[i].isDead || this.emitted[i].alpha == 0) {
+                this.emitted.splice(i, 1);
+            }
+        }
+    }
+
+    tick() {
+        let debugging = false;
+        for (let i = 0; i < this.emitted.length; i++) {
+            this.emitted[i].tick();
+        }
+        this.cleanup();
+        if (this.framecount % this.frameMod == 0 && EMIT.emitted.length < 200) {
+            this.puffcount++;
+            if (this.puffcount % this.emitRate == 0) {
+                if (debugging) {
+                    console.log(`frame: ${this.framecount} puff: ${this.puffcount}`);
+                }
+                if (this.isOn) {
+                    this.emit();
+                }
+            }
+        }
+        this.doCalculations();
+    }
+}
+
+class Smoke {
+    constructor(x, y, image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.alpha = random_range(0.5, 1).toFixed(5);
+        this.alphaDecay = 0.009;
+        this.r = (random_range(-1, 1) * Math.PI * 0.5);
+        this.vy = random_range(-1, 1);
+        this.vx = random_range(-1, 1);
+        this.lifecount = 100000 + random_range(-100, 100);
+        this.s = 1;
+        this.sMax = 100;
+        this.sFadePoint = 0.5;
+        this.scalerate = 0.08;
+        this.isDead = false;
+        this.wind = 0;
+    }
+
+    calculateSelf() {
+        this.x += this.wind;
+        this.x += this.vx;
+        this.y += this.vy;
+        this.s += this.scalerate;
+
+        if (this.r < 0) {
+            this.r -= 0.01;
+        } else {
+            this.r += 0.01;
+        }
+
+
+
+        if (this.s > this.sFadePoint) {
+            this.alpha -= this.alphaDecay;
+
+            if (this.alpha < 0) {
+                this.alpha = 0;
+            }
+        }
+
+        if (this.s > this.sMax) {
+            this.s = this.sMax;
+        }
+
+        // bound drawing to screen::
+        if (this.x < -1 * (this.image.width * this.s + 1) || this.x > canvas.width + this.image.width * this.s + 1) {
+            //            console.log(`outsideX: ${this.x}`);
+            this.isDead = true;
+        }
+        if (this.y < -1 * (this.image.height * this.s + 1) || this.y > canvas.height + this.image.height * this.s + 1) {
+            //            console.log(`outsideY: ${this.y}`);
+            this.isDead = true;
+        }
+    }
+
+    drawSelf() {
+        ctx.imageSmoothing = false;
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+
+        if (this.isDead) {
+            ctx.globalAlpha = 0.1;
+        }
+
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.r);
+        ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height,
+            (-this.image.width * this.s) / 2, (-this.image.height * this.s) / 2,
+            this.image.width * this.s, this.image.height * this.s);
+        ctx.restore();
+    }
+
+    tick() {
+        this.calculateSelf();
+        this.drawSelf();
+    }
+
+}
+
+function random_range(min, max) {
+    return Math.random() * (+max - +min) + +min;
+}
+
+function random_bool() {
+    return Math.random() >= 0.5;
+}
+
+
+// -=- Example Below is Setup and Use::
+
+function init() {
+    const smokeImage = new Image();
+    smokeImage.src = "/games/common/particles/smoke.png";
+    ctx.imageSmoothing = false;
+    return smokeImage;
+
+}
+
+function renderLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    EMIT.tick();
+    requestAnimationFrame(renderLoop);
+}
+
+const smokeImage = init();
+let EMIT = new SmokeEmitter(canvas.width / 2, canvas.height / 2, 24);
+
+document.onclick = function () {
+    EMIT.isOn = !EMIT.isOn;
+}
+
+
+renderLoop();
